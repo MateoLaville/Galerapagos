f3ff760091fd17b29beb7415f82d042a6b233348
Aude <aude.pann@hotmail.com>
2023-01-04 10:50:10 +0100

    accueil - ajout scroll bar + supression fond text

--- app/src/main/java/com/example/galerapagos/Accueil.kt
+++ app/src/main/java/com/example/galerapagos/Accueil.kt
@@ -3,13 +3,21 @@ package com.example.galerapagos
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.view.View
+import android.widget.TextView
 
 class Accueil : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_accueil)
+
+        val introductionText: TextView = findViewById(R.id.introduction)
+        introductionText.movementMethod = ScrollingMovementMethod()
+
 
         val jouer = findViewById<View>(R.id.jouer)
 
