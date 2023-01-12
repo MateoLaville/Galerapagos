4bf6b5444df6e4ec26d5c89b77d81e7da5d95f77
Aude <aude.pann@hotmail.com>
2023-01-12 16:11:01 +0100

    implémentation sauver mais ne fonctionne pas

--- app/src/main/java/com/example/galerapagos/MainActivity.kt
+++ app/src/main/java/com/example/galerapagos/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.galerapagos
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -234,5 +237,22 @@ class MainActivity : AppCompatActivity() {
             raft--
 
         raftText.text = "$raft"
+    }
+
+    /* // // // // // // //
+        // Sauver //
+    // // // // // // // */
+
+    fun takeScreenshotOfView(view: View, height: Int, width: Int): Bitmap {
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        val bgDrawable = view.background
+        if (bgDrawable != null) {
+            bgDrawable.draw(canvas)
+        } else {
+            canvas.drawColor(Color.WHITE)
+        }
+        view.draw(canvas)
+        return bitmap
     }
 }
